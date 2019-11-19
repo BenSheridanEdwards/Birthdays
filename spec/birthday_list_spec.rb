ed429f337b8455bcc85edddd6ae5745c47bca9e5
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-19 18:58:46 +0000

    Saving

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,20 +5,14 @@ describe Birthdays do
   let(:birthday) { Birthday.new }
 
   describe "store" do 
-
     it "should accept names, ages, and birth dates are arguments, and return a string confirming the information has been stored" do
       expect(subject.store("Ben", "02/03/1996")).to eq([{:birthday=>"02/03/1996", :name=>"Ben"}])
     end
   end 
 
   describe "#display" do
-
     it "should display all my friends, names, ages, and birthdays in a list" do
       expect(subject.display).to eq(subject.list)
     end
   end
-
-  describe "#today" do
-  end
-
 end
